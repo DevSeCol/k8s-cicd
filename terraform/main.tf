18cc2c549f1cc6a3c98994233b9ece68efe5b1a7
Ricardo Yepes <ricardo.yepes@servian.com>
2020-07-22 21:52:12 +1000

    Debugging

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,7 +7,7 @@ resource "google_container_cluster" "primary-cluster" {
   #subnetwork               = var.cluster_subnetwork
 }
 
-resource "google_container_node_pool" "preemptible_nodes" {
+resource "google_container_node_pool" "primary-pool" {
   name       = "primary-pool"
   location   = google_container_cluster.primary-cluster.location
   cluster    = google_container_cluster.primary-cluster.name
--- terraform/terraform.tfvars
+++ terraform/terraform.tfvars
@@ -1,4 +1,3 @@
 project_id   = "devsecol"
 region       = "australia-southeast1"
 domain_name  = "app.devsecol.site"
-cluster_name = "spark-cluster"
